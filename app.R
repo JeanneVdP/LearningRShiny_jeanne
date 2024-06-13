0550bb76e80ede2aabcbf4e4a3f6e69f78b55d11
unknown <jmms1991@gmail.com>
2024-06-13 11:26:24 +0200

    remove comments and duplication

--- app.R
+++ app.R
@@ -1,31 +1,22 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
   selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
   verbatimTextOutput("summary"),
   tableOutput("table")
 ) 
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
+  dataset <- reactive({
+    get(input$dataset, "package:datasets")
+  })
+  
   output$summary <- renderPrint({
-    dataset <- get(input$dataset, "package:datasets")
-    summary(dataset)
+    summary(dataset())
   })
   
   output$table <- renderTable({
-    dataset <- get(input$dataset, "package:datasets")
-    dataset
+    dataset()
   })
 }
 

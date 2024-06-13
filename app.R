dae5ba8dc8f4e608bff25c1953ff9ff52e6f63e1
unknown <jmms1991@gmail.com>
2024-06-13 13:01:16 +0200

    output exercise 1

--- app.R
+++ app.R
@@ -1,10 +1,17 @@
 library(shiny)
 
 ui <- fluidPage(
-  sliderInput("valuePicker", "What do you choose?", min = 0, max = 100, value = 0, step = 5, animate = TRUE)
+  #a. verbatimTextOutput("CarSummary")
+  #b. textOutput("greeting")
+  #c. verbatimTextOutput("tTest")
+  verbatimTextOutput("structureCars") #d
 ) 
 
 server <- function(input, output, session) {
+  #a.output$CarSummary <- renderPrint(summary(mtcars))
+  #b. output$greeting <- renderText("Good morning!")
+  #c. output$tTest <- renderPrint(t.test(1:5, 2:6)) 
+  output$structureCars <- renderText(str(lm(mpg~wt, data = mtcars))) #d
 }
 
 # Run the application 

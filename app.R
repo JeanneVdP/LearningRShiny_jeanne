d5612fe0dca524f570ea54ad46db65941db21244
unknown <jmms1991@gmail.com>
2024-06-14 11:35:21 +0200

    exercise 1

--- app.R
+++ app.R
@@ -1,12 +1,45 @@
 library(shiny)
 
 ui <- fluidPage(
-  dataTableOutput("dynamic")
+  textInput("name", "What's your name?"),
+  textOutput("greeting")
 ) 
 
-server <- function(input, output, session) {
-  output$dynamic <- renderDataTable(mtcars, options = list(pageLength = 5))
+# server 1
+# old:
+#server <- function(input, output, server) {
+#  input$greeting <- renderText(paste0("Hello ", name))
+#}
+
+# improved: server -> session argument, assignment of output instead of input, call input ID
+#server <- function(input, output, session) {
+#  output$greeting <- renderText(paste0("Hello ", input$name))
+#}
+
+# server 2
+# old:
+#server <- function(input, output, server){
+#  greeting <- paste0("Hello ", input$name)
+#  output$greeting <- renderText(greeting)
+#}
+
+# fixed: server -> session argument, wrap paste into reactive (to access input value), call greeting as a method
+#server <- function(input, output, session){
+#  greeting <- reactive(paste0("Hello ", input$name))
+#  output$greeting <- renderText(greeting())
+#}
+
+# server 3
+# old:
+#server <- function(input, output, server){
+#  output$greting <- paste0("Hello ", input$name)
+#}
+
+#improved: server -> session, typo in output ID, cannot assign 
+#value to output ID without render
+server <- function(input, output, session){
+  output$greeting <- renderText(paste0("Hello ", input$name))
 }
 
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server) 

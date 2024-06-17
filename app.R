e7aa60a633dfcb5b6222401a7b0d251d8dc5f19a
unknown <jmms1991@gmail.com>
2024-06-17 12:11:57 +0200

    set main panel on the right

--- app.R
+++ app.R
@@ -1,29 +1,19 @@
 library(shiny)
 library(glue)
 
-# exercise 1: determine column width of sidebarLayout
-# this is 2/3 * 12 = 8
-
-# recreate with fluid functions + columns
-#ui <- fluidPage(
-#  titlePanel("Central limit theorem"),
-#  sidebarLayout(
-#    sidebarPanel(
-#      numericInput("m", "Number of samples:", 2, min = 1, max = 100)
-#    ),
-#    mainPanel(plotOutput("hist"))
-#  )
-#)
+# exercise 2
 
 ui <- fluidPage(
   titlePanel("Central limit theorem"),
-  fluidRow(
-    column(4,
+  sidebarLayout(
+    sidebarPanel(
       numericInput("m", "Number of samples:", 2, min = 1, max = 100)
     ),
-    column(8, plotOutput("hist"))
+    mainPanel(plotOutput("hist")),
+    position = c("right")
   )
 )
+
 
 server <- function(input, output, session) {
   output$hist <- renderPlot({

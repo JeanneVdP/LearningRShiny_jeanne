1c5af45eb7c284f8b7e3b20343e367718a71321d
unknown <jmms1991@gmail.com>
2024-06-18 09:09:23 +0200

    validation

--- app.R
+++ app.R
@@ -1,34 +1,23 @@
-library(shiny)
 library(glue)
+library(shiny)
+library(shinyFeedback)
 
-# exercise 2
-
-xValues <- c(1:10)
-yValues <- c(2:11)
-zValues <- c(10:1)
 
 ui <- fluidPage(
-  titlePanel("Two plots"),
-  fluidRow(column(6, plotOutput("xAndY")),
-           column(6, plotOutput("xAndZ"))
-  ),
-  numericInput("yMultiplication", "y * input", 2, min = 1, max = 10),
-  numericInput("zMultiplication", "z * input", 5, min = 1, max = 10)
+  shinyFeedback::useShinyFeedback(),
+  numericInput("n", "n", value = 10),
+  textOutput("half")
 )
 
-
 server <- function(input, output, session) {
-  output$xAndY <- renderPlot({
-    yMultiplied <- yValues * input$yMultiplication
-    plot(xValues, yMultiplied)
-  },
-  res = 96)
+  half <- reactive({
+    isEven <- input$n %% 2 == 0
+    shinyFeedback::feedbackWarning("n", !isEven, "Please select an even number")
+    req(isEven)
+    input$n / 2
+  })
   
-  output$xAndZ <- renderPlot({
-    zMultiplied <- zValues * input$zMultiplication
-    plot(xValues, zMultiplied)
-  },
-  res = 96)
+  output$half <- renderText(half())
 }
 
 shinyApp(ui, server)

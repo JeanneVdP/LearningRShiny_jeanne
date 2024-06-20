0657ad3ce6b055b7661d9e23a2c21f1cfcf5d50e
unknown <jmms1991@gmail.com>
2024-06-20 10:12:11 +0200

    use shinycssloaders for the most simple implementation of a spinner

--- app.R
+++ app.R
@@ -8,20 +8,22 @@ library(waiter)
 ui <- fluidPage(
   waiter::use_waiter(),
   actionButton("go", "go"),
-  textOutput("result")
+  plotOutput("plot")
 )
 
 server <- function(input, output, session) {
   data <- eventReactive(input$go,{
-    waiter <- waiter::Waiter$new()
-    waiter$show()
-    on.exit(waiter$hide())
+    # use the new function to create a new waiter with an ID of 
+    # an existing element (output$plot) to overlay the waiter with
+    # (otherwise the waiter will overlay the entire body)
+    waiter::Waiter$new(id = "plot")$show()
+    # notice how on.exit is no longer needed
     
-    Sys.sleep(sample(5, 1))
-    runif(1)
+    Sys.sleep(3)
+    data.frame(x = runif(50), y = runif(50))
   })
   
-  output$result <- renderText(round(data(), 2))
+  output$plot <- renderPlot(plot(data(), res = 96))
 }
 
 shinyApp(ui, server)

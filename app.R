49a68405b86867e04f9e0721c10d6f6e04245a8f
Jeanne <jmms1991@gmail.com>
2024-06-21 17:25:42 +0200

    exercise 1: create hidden additional controls with tabs

--- app.R
+++ app.R
@@ -1,30 +1,41 @@
-library(glue)
 library(shiny)
-library(dplyr, warn.conflicts = FALSE)
-
-
 
 ui <- fluidPage(
-  tabsetPanel(id = "wizard",
-              type = "hidden",
-              tabPanel("page_1", "welcome!", actionButton("page_12", "next")),
-              tabPanel("page_2", 
-                       "Only one page to go!", 
-                       actionButton("page_21", "prev"),
-                       actionButton("page_23", "next")),
-              tabPanel("page_3", "You're done!", actionButton("page_32", "prev")),
+  sidebarLayout(
+    sidebarPanel(
+      checkboxInput("moreControls", 
+                    label = "Show advanced controls?",
+                    value = FALSE
+      )
+    ),
+    mainPanel(
+      tabsetPanel(
+        id = "basic",
+        type = "hidden",
+        tabPanelBody("panel1",
+                     numericInput("basicControl", label = "Basic:", 0),
+        )
+      ),
+      tabsetPanel(
+        id = "advanced",
+        type = "hidden",
+        tabPanelBody("emptyPanel", style = "display: none"),
+        tabPanelBody("panel2",
+                     numericInput("advancedCotrol", label = "Advanced:", 1)
+        )
+      )
     )
+  )
 )
 
 server <- function(input, output, session) {
-  switchPage <- function(i){
-    updateTabsetPanel(inputId = "wizard", selected = paste0("page_", i))
-   }
-  
-  observeEvent(input$page_12, switchPage(2))
-  observeEvent(input$page_21, switchPage(1))
-  observeEvent(input$page_23, switchPage(3))
-  observeEvent(input$page_32, switchPage(2))
+  observeEvent(input$moreControls, {
+    if (input$moreControls) {
+      updateTabsetPanel(session, "advanced", selected = "panel2")
+    } else {
+      updateTabsetPanel(session, "advanced", selected = "emptyPanel")
+    }
+  })
 }
 
 shinyApp(ui, server)
